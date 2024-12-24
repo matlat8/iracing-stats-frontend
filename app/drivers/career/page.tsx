966f31b0eb2e802516295875fe81365e47403afc
Matthew Latham <50916823+matlat8@users.noreply.github.com>
2024-12-24 18:08:27 -0500

    feat: Add lazy loading for images on Career Page

--- app/drivers/career/page.tsx
+++ app/drivers/career/page.tsx
@@ -1,10 +1,9 @@
 'use client'
 
+import { Suspense } from 'react';
 import { useRouter, useSearchParams } from "next/navigation";
 
-export default function CareerPage() {
-
-    
+function CareerPageContent() {
     const searchParams = useSearchParams();
     const cust_id = searchParams.get('cust_id');
     const router = useRouter();
@@ -15,9 +14,18 @@ export default function CareerPage() {
         router.push('/');
         return null;
     }
+
     return (
         <div>
-        <h1>Career Page {cust_id}</h1>
+            <h1>Career Page {cust_id}</h1>
         </div>
-    )
+    );
+}
+
+export default function CareerPage() {
+    return (
+        <Suspense fallback={<div>Loading...</div>}>
+            <CareerPageContent />
+        </Suspense>
+    );
 }
