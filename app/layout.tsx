c25fc3f33cc0c710e07ab6b247d77581b934b6f5
Matthew Latham <50916823+matlat8@users.noreply.github.com>
2024-12-24 20:20:52 -0500

    build errors

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,4 @@
-import { SidebarInset, SidebarProvider, SidebarTrigger } from "~/components/ui/sidebar"
+import { SidebarProvider } from "~/components/ui/sidebar"
 import { ThemeProvider } from "next-themes";
 import { AppSidebar } from "~/components/app-sidebar"
 import "./globals.css";
