ae0252215b7d57c18cd758f2b31e86a8ab4080cb
Matthew Latham <50916823+matlat8@users.noreply.github.com>
2024-12-25 12:49:18 -0500

    quick links on the home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 
 import type { Metadata } from 'next'
+import Link from 'next/link'
 
 export const metadata: Metadata = {
   title: 'Home',
@@ -8,6 +9,32 @@ export const metadata: Metadata = {
 
 }
 
+const quickDrivers = [{
+  title: 'Matt Malone',
+  custId: 120570,
+}, {
+  title: 'Oliver Furnell',
+  custId: 510501
+}]
+
 export default function Home() {
-  return (<p>Hello!</p>);
+  return (
+    <div className='flex items-center mt-12 w-full'>
+      {quickDrivers.map((driver, index) => (
+        <QuickDriverCard key={index} title={driver.title} custId={driver.custId} />
+      ))}
+
+    </div>
+  );
+}
+
+function QuickDriverCard({ title, custId }: { title: string, custId: number }) {
+  return (
+    <Link href={`/drivers/career?cust_id=${custId}`}>
+      <div className='bg-white border border-gray-200 h-16 rounded-sm m-4 p-4'>
+        {title}
+      </div>
+    </Link>
+  )
+
 }
